c0b990c75c40a47bd67e9bf7f7368e98ec8176a9
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-05-21 15:56:13 +0700

    refactor(account-menu): Improve account menu component
    The "account-menu.tsx" component has been refactored to improve its structure and readability. Changes include:
    
    - Updated import statements to use consistent formatting.
    - Adjusted the indentation and spacing for better code organization.
    - Replaced double quotes with single quotes for string literals.
    - Added a `Link` component from Next.js to wrap the logout button for better navigation.
    - Applied consistent formatting for button variants.
    
    These changes aim to enhance the clarity and maintainability of the account menu component.

--- components/account-menu.tsx
+++ components/account-menu.tsx
@@ -1,50 +1,49 @@
-import React from 'react'
+import React from "react"
+import Link from "next/link"
+
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { Icons } from './icons'
-import Link from 'next/link'
-import { buttonVariants } from './ui/button'
+
+import { Icons } from "./icons"
+import { buttonVariants } from "./ui/button"
 
 export default function AccountMenu() {
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger>
-                <Icons.userCircle className="h-5 w-5 fill-current" />
-                <span className="sr-only">Account</span>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-                <DropdownMenuLabel className='text-center'>
-                    <span className="block">My Account - Budi</span>
-                    <span className='text-xs'>Administrator</span>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Profile</DropdownMenuItem>
-                <DropdownMenuItem>Team</DropdownMenuItem>
-                <DropdownMenuItem>Settings</DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                    <Link
-                        href={'/'}
-                        rel="noreferrer"
-                    >
-                        <div
-                            className={buttonVariants({
-                                size: "sm",
-                                variant: "ghost",
-                            })}
-                        >
-                            <Icons.logout className="h-4 w-4 mr-1 fill-current" />
-                            <span>Logout</span>
-                        </div>
-                    </Link>
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
-    )
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Icons.userCircle className="h-5 w-5 fill-current" />
+        <span className="sr-only">Account</span>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel className="text-center">
+          <span className="block">My Account - Budi</span>
+          <span className="text-xs">Administrator</span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Settings</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Link href={"/"} rel="noreferrer">
+            <div
+              className={buttonVariants({
+                size: "sm",
+                variant: "ghost",
+              })}
+            >
+              <Icons.logout className="h-4 w-4 mr-1 fill-current" />
+              <span>Logout</span>
+            </div>
+          </Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
 }
