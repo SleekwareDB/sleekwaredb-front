73ba7f1ea4c3a51a4cb8c9bc61b9ab188010aeff
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-05-21 15:56:59 +0700

    feat(dashboard): Add navigation links
    The "page.tsx" component in the dashboard section has been updated to include navigation links. Changes include:
    
    - Added import statements for the "Link" component from Next.js.
    - Updated the component structure to use CSS grid with 12 columns.
    - Added a left sidebar with links for different sections, such as "Database," "Team Members," "Users," and "Permission."
    - Added a main content area with a heading "Dashboard."
    
    These changes aim to enhance the user experience by providing navigation options within the dashboard page.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react"
+import Link from "next/link"
 
 export default function dashboard() {
   return (
-    <section className="container pt-2">
-      <h1>Dashboard</h1>
+    <section className="grid grid-cols-12">
+      <div className="col-span-2 py-4 px-2 border border-l-0 border-t-0 border-b-0 dark:border-gray-600 border-gray-200">
+        <div className="space-y-2">
+          <Link
+            href={"javascript:"}
+            className={`inline-flex w-full text-sm dark:bg-slate-800 bg-slate-200 text-left py-2 px-3 rounded font-bold`}
+          >
+            Database
+          </Link>
+          <Link
+            href={"javascript:"}
+            className={`inline-flex w-full text-sm text-left py-2 px-3 rounded font-bold hover:dark:bg-slate-800 hover:bg-slate-200`}
+          >
+            Team Members
+          </Link>
+          <Link
+            href={"javascript:"}
+            className={`inline-flex w-full text-sm text-left py-2 px-3 rounded font-bold hover:dark:bg-slate-800 hover:bg-slate-200`}
+          >
+            Users
+          </Link>
+          <Link
+            href={"javascript:"}
+            className={`inline-flex w-full text-sm text-left py-2 px-3 rounded font-bold hover:dark:bg-slate-800 hover:bg-slate-200`}
+          >
+            Permission
+          </Link>
+        </div>
+      </div>
+      <div className="col-auto py-4 px-2">
+        <h1>Dashboard</h1>
+      </div>
     </section>
   )
 }
